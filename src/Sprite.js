cf7bf53955e5002876b4161a8345ff03136ffdb2
bendem <online@bendem.be>
2015-04-15 18:32:33 +0200

    Add sprite utility

---
+++ src/Sprite.js
@@ -0,0 +1,42 @@
+/**
+ * Creates an utility to manage sprites.
+ *
+ * @param Image The image data of the sprite
+ */
+var Sprite = function(img) {
+    this.img = img;
+    this.descriptors = [];
+};
+
+Sprite.prototype = {
+    registerId: function(id, position, width, height) {
+        this.descriptors.push({
+            id: id,
+            position: position,
+            width: width,
+            height: heigh,
+        });
+
+        return this;
+    },
+
+    draw: function(ctx, id, position) {
+        var spritePart = search(this.descriptors, 'id', id);
+        if(spritePart === null) {
+            return this;
+        }
+
+        ctx.drawImage(
+            this.sprite,
+            spritePart.position.x,
+            spritePart.position.y,
+            spritePart.position.width,
+            spritePart.position.height,
+            position.x,
+            position.y,
+            spritePart.position.width,
+            spritePart.position.height
+        );
+        return this;
+    },
+};
